17e44d9f209faa2d7a7766930375b2cd3dd37856
one031411 <one031411@naver.com>
2023-05-12 17:43:58 +0900

    컴퓨터와 가위바위보 프로그램출력

---
+++ t20230510/ComputerPlayer.java
@@ -0,0 +1,10 @@
+package t20230510;
+
+public class ComputerPlayer {
+
+	public int nextHand() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ t20230510/HumanPlayer.java
@@ -0,0 +1,10 @@
+package t20230510;
+
+public class HumanPlayer {
+
+	public int nextHand() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
